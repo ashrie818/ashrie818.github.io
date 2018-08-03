5011bed005e77a551e1f1d866cf6cface61a5aed
ashrie818 <42047001+ashrie818@users.noreply.github.com>
2018-08-03 15:29:26 -0500

    Add files via upload

--- coding101/script.js
+++ coding101/script.js
@@ -1,28 +1,16 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+<button onclick="two()">2</button>
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+<script>
+// Get the elements with class="column"
+var elements = document.getElementsByClassName("column");
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+// Declare a "loop" variable
+var i;
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1} 
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none"; 
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block"; 
-  dots[slideIndex-1].className += " active";
+// Two images side by side
+function two() {
+    for (i = 0; i < elements.length; i++) {
+        elements[i].style.flex = "50%"; 
+    }
 }
+</script>
